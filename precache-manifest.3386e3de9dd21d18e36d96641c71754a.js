d7ff847707e8907243c856052c62f6e39f9a81e6
Victor Ughonu <victorughonu95@gmail.com>
2018-10-10 23:03:26 +0100

    Updates

---
+++ precache-manifest.3386e3de9dd21d18e36d96641c71754a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "53391f2d577cc4a40884",
+    "url": "/contactmanager/static/js/main.53391f2d.chunk.js"
+  },
+  {
+    "revision": "9d8e088024a57a22d5f2",
+    "url": "/contactmanager/static/js/1.9d8e0880.chunk.js"
+  },
+  {
+    "revision": "53391f2d577cc4a40884",
+    "url": "/contactmanager/static/css/main.bf5b33eb.chunk.css"
+  },
+  {
+    "revision": "9d8e088024a57a22d5f2",
+    "url": "/contactmanager/static/css/1.f6b6874c.chunk.css"
+  },
+  {
+    "revision": "c3b7f244e8e3864a09858ce24518c453",
+    "url": "/contactmanager/index.html"
+  }
+];
